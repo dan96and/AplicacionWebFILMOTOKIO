07445ef1132267f5a165ffccf06b3b9f7963205f
Daniel <d.andres.bacho@gmail.com>
2023-05-31 14:50:27 +0200

    Initial commit

---
+++ src/main/java/com/example/aplicacionwebfilmotokio/AplicacionWebFilmotokioApplication.java
@@ -0,0 +1,14 @@
+package com.example.aplicacionwebfilmotokio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionWebFilmotokioApplication {
+
+	public static void main(String[] args) {
+		System.out.println("DANIEL");
+		SpringApplication.run(AplicacionWebFilmotokioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aplicacionwebfilmotokio/AplicacionWebFilmotokioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionwebfilmotokio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionWebFilmotokioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
