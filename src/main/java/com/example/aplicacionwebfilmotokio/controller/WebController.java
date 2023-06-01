986a08a040b667e80dc5855b6a40b41511174470
Daniel <d.andres.bacho@gmail.com>
2023-06-01 19:29:20 +0200

    Creación de las pantallas de Home y Login

---
+++ src/main/java/com/example/aplicacionwebfilmotokio/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.example.aplicacionwebfilmotokio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String footer() {
+        return "index";
+    }
+}
